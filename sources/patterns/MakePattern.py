8ee5522b6e881d565aac53bdab913f050beccc9c
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2018-11-23 21:44:35 +0330

    english dictionary and word pattern added

---
+++ sources/patterns/MakePattern.py
@@ -0,0 +1,9 @@
+import os
+
+from lib.MakeWordPattern import make_word_pattern
+
+base = os.path.abspath(__file__)
+
+
+def make_pattern(input_path: str, output_name: str):
+    make_word_pattern(input_path, os.path.join(base, output_name + '.py'))
