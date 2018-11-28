4e2aa916254d0665d5d0337f01bff1d2dff55beb
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2018-11-28 20:03:31 +0330

    refactored

--- sources/lib/WordPatternAnalyzer.py
+++ sources/lib/WordPatternAnalyzer.py
@@ -1,75 +1,22 @@
 import copy
-from lib.Characters import LETTERS
-from lib import MakeWordPattern
+from collections import defaultdict
+
+from lib import WordPatternMaker
 from lib import wordPatterns
+from lib.Characters import LETTERS
 
 
-def main():
+def frequency_aided_mapper(pattern: dict, word_frequency: str, complex_frequency: tuple):
     pass
 
 
-def get_blank_cipherletter_mapping() -> dict:
-    return {'A': set(), 'B': set(), 'C': set(), 'D': set(), 'E': set(), 'F': set(), 'G': set(), 'H': set(), 'I': set(),
-            'J': set(), 'K': set(), 'L': set(), 'M': set(), 'N': set(), 'O': set(), 'P': set(), 'Q': set(), 'R': set(),
-            'S': set(), 'T': set(), 'U': set(), 'V': set(), 'W': set(), 'X': set(), 'Y': set(), 'Z': set(), 'a': set(),
-            'b': set(), 'c': set(), 'd': set(), 'e': set(), 'f': set(), 'g': set(), 'h': set(), 'i': set(), 'j': set(),
-            'k': set(), 'l': set(), 'm': set(), 'n': set(), 'o': set(), 'p': set(), 'q': set(), 'r': set(), 's': set(),
-            't': set(), 'u': set(), 'v': set(), 'w': set(), 'x': set(), 'y': set(), 'z': set(), '!': set(), '"': set(),
-            '#': set(), '$': set(), '%': set(), '&': set(), '\'': set(), '(': set(), ')': set(), '*': set(), '+': set(),
-            ',': set(), '-': set(), '.': set(), '/': set(), '0': set(), '1': set(), '2': set(), '3': set(), '4': set(),
-            '5': set(), '6': set(), '7': set(), '8': set(), '9': set(), ':': set(), ';': set(), '<': set(), '=': set(),
-            '>': set(), '?': set(), '@': set(), '[': set(), '\\': set(), ']': set(), '^': set(), '_': set(), '{': set(),
-            '|': set(), '}': set(), '~': set(), ' ': set()}
-
-
-def add_letters_to_mapping(letter_mapping: dict, cipher_word: str, candidate: list):
-    letter_mapping = copy.deepcopy(letter_mapping)
-    for i in range(len(cipher_word)):
-        letter_mapping[cipher_word[i]].add(candidate[i].upper())
-
-    return letter_mapping
-
-
-def intersect_mappings(map_a: dict, map_b: dict) -> dict:
-    intersected_mapping = get_blank_cipherletter_mapping()
-    for letter in LETTERS:
-        if not map_a[letter]:
-            intersected_mapping[letter] = copy.deepcopy(map_b[letter])
-        elif not map_b[letter]:
-            intersected_mapping[letter] = copy.deepcopy(map_a[letter])
-        else:
-            for mappedLetter in map_a[letter]:
-                if mappedLetter in map_b[letter]:
-                    intersected_mapping[letter].add(mappedLetter)
-
-    return intersected_mapping
-
-
-def remove_solved_letters_from_mapping(letter_mapping: dict):
-    letter_mapping = copy.deepcopy(letter_mapping)
-    loop_again = True
-    while loop_again:
-        loop_again = False
-        solved_letters = list()
-        for cipherletter in LETTERS:
-            if len(letter_mapping[cipherletter]) == 1:
-                solved_letters.append(next(iter(letter_mapping[cipherletter])))
-        for cipherletter in LETTERS:
-            for s in solved_letters:
-                if len(letter_mapping[cipherletter]) != 1 and s in letter_mapping[cipherletter]:
-                    letter_mapping[cipherletter].remove(s)
-                    if len(letter_mapping[cipherletter]) == 1:
-                        loop_again = True
-
-    return letter_mapping
-
-
-def hack_simple_sub(message):
-    intersected_map = get_blank_cipherletter_mapping()
-    cipher_word_list = message.split()
+def pattern_mapper(string: str) -> dict:
+    """ Function to produce letter mapping for an string"""
+    intersected_map = blank_mapper_dict()
+    cipher_word_list = string.split()
     for cipher_word in cipher_word_list:
-        new_map = get_blank_cipherletter_mapping()
-        word_pattern = MakeWordPattern.get_word_pattern(cipher_word)
+        new_map = blank_mapper_dict()
+        word_pattern = WordPatternMaker.get_word_pattern(cipher_word)
         if word_pattern not in wordPatterns.allPatterns:
             continue
         for candidate in wordPatterns.allPatterns[word_pattern]:
@@ -80,5 +27,84 @@ def hack_simple_sub(message):
     return remove_solved_letters_from_mapping(intersected_map)
 
 
-if __name__ == '__main__':
-    main()
+def blank_mapper_dict() -> dict:
+    """ Function that returns a dictionary containing
+        all letters as keys and blank set as value"""
+
+    dictionary = defaultdict(set)
+    # populate dictionary
+    for letter in LETTERS:
+        dictionary[letter]
+    return dictionary
+
+
+def add_letters_to_mapping(letter_mapping: dict, string: str, candidate: list) -> dict:
+    """ Function to create map between a string and its candidate"""
+    letter_mapping = copy.deepcopy(letter_mapping)
+    # map
+    for i in range(len(string)):
+        # TODO : candidate isn't upper()
+        letter_mapping[string[i]].add(candidate[i].upper())
+
+    return letter_mapping
+
+
+def intersect_mappings(map_a: dict, map_b: dict) -> dict:
+    """ Function to calculate intersected mappings out of 2 maps"""
+    # new mapper dictionary
+    intersected_mapping = blank_mapper_dict()
+
+    for letter in LETTERS:
+        # if map_a[letter] is empty, copy map_b[letter] into intersected mapping
+        if not map_a[letter]:
+            intersected_mapping[letter] = copy.deepcopy(map_b[letter])
+        # if map_b[letter] is empty, copy map_a[letter] into intersected mapping
+        # if both map_a and map_b are empty then the intersected mapping will be empty
+        elif not map_b[letter]:
+            intersected_mapping[letter] = copy.deepcopy(map_a[letter])
+        # if map_a and map_b both has values, then only copy the values that two maps
+        # have in common in "both" maps into intersected mapper
+        else:
+            for mapped_letter in map_a[letter]:
+                if mapped_letter in map_b[letter]:
+                    intersected_mapping[letter].add(mapped_letter)
+
+    return intersected_mapping
+
+
+def remove_solved_letters_from_mapping(letter_mapping: dict) -> dict:
+    """
+    Letters in the mapping that map to only one letter are
+    "solved" and can be removed from the other letters.
+    For example, if 'A' maps to potential letters ['M', 'N'], and 'B'
+    maps to ['N'], then we know that 'B' must map to 'N', so we can
+    remove 'N' from the list of what 'A' could map to. So 'A' then maps
+    to ['M']. Note that now that 'A' maps to only one letter, we can
+    remove 'M' from the list of potential letters for every other
+    key. (This is why there is a loop that keeps reducing the map.)
+    """
+    letter_mapping = copy.deepcopy(letter_mapping)
+    # loop
+    loop = True
+    while loop:
+        # first assume that we will not loop again
+        loop = False
+        # solvedLetters will be a list of letters that have one
+        # and only one possible mapping in letter_mapping
+        solved_letters = list()
+        # loop through letters and find solved ones
+        for letter in LETTERS:
+            if len(letter_mapping[letter]) == 1:
+                # append letter to solved letters
+                solved_letters.append(next(iter(letter_mapping[letter])))
+        # loop through letters and remove solved ones from mapping
+        for letter in LETTERS:
+            for solved in solved_letters:
+                if len(letter_mapping[letter]) != 1 and solved in letter_mapping[letter]:
+                    letter_mapping[letter].remove(solved)
+
+                    # A new letter is now solved, so loop again.
+                    if len(letter_mapping[letter]) == 1:
+                        loop = True
+
+    return letter_mapping
